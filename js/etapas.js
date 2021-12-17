c934fb27ec478a33d6cbb25f2fe522ae498e8c84
allysoncsc <allysoncsc@gmail.com>
2021-12-16 22:30:13 -0300

    comecando JS

---
+++ js/etapas.js
@@ -0,0 +1,43 @@
+let etapas = [
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: 11,
+            nome: 'Goku',
+            partido: 'DBZ',
+            foto:[ {url:'../imagens/goku.jpg', legenda: 'prefeito'},
+            {url:'../imagens/vegeta.jpg', legenda: 'vice-prefeito'}   ]
+
+          },
+
+          {
+            numero: 22,
+            nome: 'Seya',
+            partido: 'CAVALEIROS',
+            foto:[ {url:'../imagens/seya.jpg', legenda: 'prefeito'},
+            {url:'../imagens/shiryu.jpg', legenda: 'vice-prefeito'}   ]
+
+          },
+            {
+                numero: 33,
+                nome: 'Saitama',
+                partido: 'puchman',
+                foto:[ {url:'../imagens/saitama.jpg', legenda: 'prefeito'},
+                {url:'../imagens/genus.jpg', legenda: 'vice-prefeito'}   ]
+
+              },
+              {
+                numero: 44,
+                nome: 'Ichigo',
+                partido: 'BLEACH',
+                foto:[ {url:'../imagens/ichigo.jpg', legenda: 'prefeito'},
+                {url:'../imagens/urahara.jpg', legenda: 'vice-prefeito'}   ]
+
+              }
+
+          ]
+        }
+
+]
